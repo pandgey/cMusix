0e2c40236c27a0f3513b6d8ce8ea5a6299a58593
pandgey <krittapong.ti@gmail.com>
2025-07-15 11:32:53 +0700

    added some input

--- input.c
+++ input.c
@@ -1,3 +1,49 @@
+#include "cMusix.h"
+
+// Handle keyboard input
 void handle_input() {
+    char ch = getchar();
+    if (ch == EOF) return;
     
-};
+    switch (ch) {
+        case ' ':
+            if (!player.is_playing && player.count > 0) {
+                play_current_song();
+            } else {
+                pause_resume();
+            }
+            break;
+        case 'n':
+            next_song();
+            break;
+        case 'p':
+            previous_song();
+            break;
+        case '+':
+        case '=':
+            set_volume(player.volume + 0.1f);
+            break;
+        case '-':
+            set_volume(player.volume - 0.1f);
+            break;
+        case 's':
+            toggle_shuffle();
+            break;
+        case 'r':
+            toggle_repeat();
+            break;
+        case 'q':
+            exit(0);
+            break;
+        case 'j':
+            if (player.list_offset + DISPLAY_SONGS < player.count) {
+                player.list_offset++;
+            }
+            break;
+        case 'k':
+            if (player.list_offset > 0) {
+                player.list_offset--;
+            }
+            break;
+    }
+}
